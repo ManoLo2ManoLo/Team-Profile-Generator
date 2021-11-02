376727c645f36addf48c2fa2cc8fb6f1fc9db653
ManoLo2ManoLo <manuel_canas@aol.com>
2021-11-02 08:48:33 -0400

    retyping all the code i previously had

---
+++ index.js
@@ -0,0 +1,168 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/page-template')
+
+const Employee = require('./lib/Employee.js')
+const Manager = require('./lib/Manager.js')
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
+const teamArray = [];
+
+const managerQuestion = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the team manager's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the team manager's email?",
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the team member's office number?",
+        }, 
+        {
+            type: 'confirm',
+            name: 'confirm',
+            message: "Would you like to add a team member?",
+            default: false
+        },
+    ]).then(function(data){
+        let {name, id, email, office, confirm} = data;
+        let manager = new Manager(name, id, email, office);
+
+        teamArray.push(manager);
+
+        if (confirm) {
+            console.log('\n--------------------------------------\n')
+            employeeQuestion();
+        } else {
+            writeToFile('./dist/index.html', generatePage(data));
+        }
+    })
+}
+
+const employeeQuestion = () => { 
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the team member's role?",
+            choices: ['Engineer', 'Intern']
+        },
+    ]).then(function(data){
+        let {role} = data;
+
+        if (role === 'Engineer') {
+            engineerQuestion();
+        } else if (role === 'Intern') {
+            internQuestion()
+        }
+    })
+}
+
+const engineerQuestion = () => { 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the engineer's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the engineer's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the engineer's email?",
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's github?",
+        }, 
+        {
+            type: 'confirm',
+            name: 'confirm',
+            message: "Would you like to add a team member?",
+            default: false
+        },
+    ]).then(function(data){
+        let {name, id, email, github, confirm} = data;
+        let employee = new Engineer(name, id, email, github);
+
+        teamArray.push(employee);
+
+        if (confirm) {
+            console.log('\n--------------------------------------\n')
+            employeeQuestion();
+        } else {
+            writeToFile('./dist/index.html', generatePage(teamArray));
+        }
+    })
+}
+
+const internQuestion = () => { 
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the intern's name?",
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the intern's ID number?",
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the intern's email?",
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What is the intern's school?",
+        }, 
+        {
+            type: 'confirm',
+            name: 'confirm',
+            message: "Would you like to add a team member?",
+            default: false
+        },
+    ]).then(function(data){
+        let {name, id, email, school, confirm} = data;
+        let employee = new Intern(name, id, email, school);
+
+        teamArray.push(employee);
+
+        if (confirm) {
+            console.log('\n--------------------------------------\n')
+            employeeQuestion();
+        } else {
+            writeToFile('./dist/index.html', generatePage(teamArray));
+        }
+    })
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err) {
+        if(err) {
+            console.log(err);
+        }
+    })
+}
+
+managerQuestion();
---
+++ src/page-template.js
@@ -0,0 +1,103 @@
+const copyFile = require('../utils/generate-site');
+
+function generateIcon(data) {
+  if (data === 'Engineer') {
+    return `fas fa-glasses`
+  } else {
+    return `fas fa-user-graduate`
+  }
+}
+
+function generateTeam(data) {
+  if (data.github) {
+    return `<div class="col s6">
+          <div class="card">
+            <div class="card-content white-text">
+              <span class="title">${data.name}</span>
+              <p class="icon"><i class="fas fa-glasses icon"></i> Engineer</p>
+            </div>
+            <div class="card-action">
+              <p class="white-text">ID: ${data.id}</p>
+              <p class="white-text">Email: <a href="mailto: ${data.email}" target="_blank">${data.email}</a></p>
+              <p class="white-text">Github: <a href="https://www.github.com/${data.github}" target="_blank">${data.github}</a></p>
+            </div>
+          </div>
+        </div>`
+  } else if (data.school) {
+    return `<div class="col s6">
+          <div class="card">
+            <div class="card-content white-text">
+              <span class="title">${data.name}</span>
+              <p class="icon"><i class="fas fa-user-graduate icon"></i> Intern</p>
+            </div>
+            <div class="card-action">
+              <p class="white-text">ID: ${data.id}</p>
+              <p class="white-text">Email: <a href="mailto: ${data.email}" target="_blank">${data.email}</a></p>
+              <p class="white-text">School: ${data.school}</p>
+            </div>
+          </div>
+        </div>`
+  }
+}
+
+function printTeamLayout(data) {
+  let print = ''
+  if (data.length > 1) {
+    for (let i = 1; i < data.length; i++) {
+      print += `
+        ${generateTeam(data[i])}
+      `
+    }
+    return print;
+  } else {
+    return ' '
+  }
+}
+
+function generatePage(data) {
+  copyFile() 
+  return `<!DOCTYPE html>
+          <html lang="en">
+            <head>
+              <meta charset="UTF-8">
+              <meta http-equiv="X-UA-Compatible" content="IE=edge">
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <!-- Compiled and minified CSS -->
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+              <link rel="stylesheet" href="style.css">
+              <title>My Team</title>
+            </head>
+            <body>
+              <nav>
+                <div class="nav-wrapper">
+                  <a class="brand-logo center">My Team</a>
+                </div>
+              </nav>
+
+              <div class="container">
+                <div class="row center">
+                  <div class="col s6">
+                    <div class="card">
+                      <div class="card-content white-text">
+                        <span class="title">${data[0].name}</span>
+                        <p class="icon"><i class="fas fa-mug-hot icon"></i> Manager</p>
+                      </div>
+                      <div class="card-action">
+                        <p class="white-text">ID: ${data[0].id}</p>
+                        <P class="white-text">Email: <a href="mailto: ${data[0].email}" target="_blank">${data[0].email}</a></P>
+                        <p class="white-text">Office Number: ${data[0].office}</p>
+                      </div>
+                    </div>
+                  </div>
+                  ${printTeamLayout(data)}
+                </div>
+              </div>
+        
+              <!-- Compiled and minified JavaScript -->
+              <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            </body>
+          </html>`
+}
+
+module.exports = generatePage;
